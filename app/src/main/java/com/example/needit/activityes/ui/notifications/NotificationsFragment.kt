2179600eebdc5b7870ded1a60ba6ac1bd5e26181
Dmitry <dmitry.ananev2000@gmail.com>
2022-06-25 00:30:37 +0700

    Сделал рефракторинг кода

--- app/src/main/java/com/example/needit/activityes/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/needit/activityes/ui/notifications/NotificationsFragment.kt
@@ -4,12 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.needit.R
-import com.example.needit.activityes.ui.dashboard.PersonRequest
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.fragment_notifications.*
 
 class NotificationsFragment : Fragment() {
@@ -24,29 +20,13 @@ class NotificationsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val db = Firebase.firestore
+        /*lateinit var notificationsViewModel: NotificationsViewModel
+        notificationsViewModel = ViewModelProvider(this)[NotificationsViewModel::class.java]*/
         button_submit.setOnClickListener {
-            val NeedOrGive: String  // Проверка на свич (Give или Need)
-            if (switch1.isChecked) {
-                NeedOrGive = "Give"
-            } else {
-                NeedOrGive = "Need"
-            }
-
-            val personalRequest = PersonRequest(1,  // Создаём объект PersonalRequest
-                editTextTextPersonName.text.toString(),
-                "Null",
-                editTextTextMultiLine.text.toString(),
-                NeedOrGive,
-                "Lenina, 53"
-            )
-            db.collection("Stuff")      // Добавляем его в БД
-                .document(editTextTextPersonName
-                    .text.toString())
-                .set(personalRequest)
-            Toast.makeText(activity, "Заявка создана", Toast.LENGTH_SHORT).show()
-            editTextTextMultiLine.setText("")
-            editTextTextPersonName.setText("")
+            button_submit.setText(R.string.Retry1)
+            editTextTextMultiLine.visibility = View.GONE
+            editTextTextPersonName.visibility = View.GONE
+            switch1.visibility= View.GONE
         }
 
     }
